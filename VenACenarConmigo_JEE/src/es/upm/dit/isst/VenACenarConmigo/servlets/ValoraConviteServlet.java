ba9c1fa0e43c6e54bd81fdeba76237d102f5f82c
Luismvp <luismvp96@gmail.com>
2018-05-20 16:20:04 +0200

    valoraciones reparadas

--- VenACenarConmigo_JEE/src/es/upm/dit/isst/VenACenarConmigo/servlets/ValoraConviteServlet.java
+++ VenACenarConmigo_JEE/src/es/upm/dit/isst/VenACenarConmigo/servlets/ValoraConviteServlet.java
@@ -21,11 +21,11 @@ public class ValoraConviteServlet extends HttpServlet {
 		String[] invitados = new String[15];
 		int[] valoracion = new int[15];
 		String[] comentarios = new String[15];
-		int idValoracion=0;
+		int idValoracion= ValoracionDAOImplementation.getInstance().readAllValoracion().size();
 		Valoracion valoracionFinal = new Valoracion();
 		for(int i=1;i<=15;i++) {
 			if(req.getParameter("email"+i)!=null) {
-				invitados[i-1]=req.getParameter("email"+1);
+				invitados[i-1]=req.getParameter("email"+i);
 			}else {
 				invitados[i-1]="0";
 			}
@@ -43,15 +43,12 @@ public class ValoraConviteServlet extends HttpServlet {
 			}
 			log("comentarios "+comentarios[i-1]);
 		}
-		if(ValoracionDAOImplementation.getInstance().readAllValoracion().size()==0) {
-			idValoracion=1;
-		}else {
-			idValoracion= ValoracionDAOImplementation.getInstance().readAllValoracion().size()+1;
-		}
 		String emailA= "";
 		String comentA="";
 		int valorA=0;
 		if(req.getParameter("emailAnfitrion")!=null) {
+			idValoracion++;
+			log(""+idValoracion);
 			emailA= (String)req.getParameter("emailAnfitrion");
 			comentA= (String) req.getParameter("comentarioAnfitrion");
 			valorA = Integer.parseInt(req.getParameter("carasAnfi"));
@@ -61,10 +58,12 @@ public class ValoraConviteServlet extends HttpServlet {
 			valoracionFinal.setPuntuacion(valorA);
 			valoracionFinal.setUsuarioValorado(emailA);
 			valoracionFinal.setUsuarioValorador(email);
-			idValoracion++;
+			ValoracionDAOImplementation.getInstance().createValoracion(valoracionFinal);
 		}
 		for(int i=0;i<invitados.length;i++) {
 			if(!invitados[i].equals("0") && valoracion[i]!=0 && !comentarios[i].equals("0")) {
+				idValoracion++;
+				log(""+idValoracion);
 				valoracionFinal.setComentario(comentarios[i]);
 				valoracionFinal.setConvite(idConvite);
 				valoracionFinal.setIdValoracion(idValoracion);
@@ -72,7 +71,7 @@ public class ValoraConviteServlet extends HttpServlet {
 				valoracionFinal.setUsuarioValorado(invitados[i]);
 				valoracionFinal.setUsuarioValorador(email);
 				ValoracionDAOImplementation.getInstance().createValoracion(valoracionFinal);
-				idValoracion++;
+				
 			}
 		}
 		resp.sendRedirect(req.getContextPath()+"/Convite.jsp");
